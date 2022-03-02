5bfb5150cd13c49da99970aaaa99c300a265895e
skybax <tmflame@gmail.com>
2022-03-01 20:41:52 -0600

    Lay out class for managing active cups

--- cup_manager/__init__.py
+++ cup_manager/__init__.py
@@ -5,6 +5,7 @@ from pyplanet.apps.config import AppConfig
 from .results import ResultsCupManager
 from .setup import SetupCupManager
 from .payouts import PayoutCupManager
+from .cup  import CupCupManager
 
 logger = logging.getLogger(__name__)
 
@@ -20,9 +21,11 @@ class CupManagerApp(AppConfig):
 		self.results = ResultsCupManager(self)
 		self.setup = SetupCupManager(self)
 		self.payout = PayoutCupManager(self)
+		self.cup = CupCupManager(self)
 
 
 	async def on_start(self):
 		await self.results.on_start()
 		await self.setup.on_start()
 		await self.payout.on_start()
+		await self.cup.on_start()
