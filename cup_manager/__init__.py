733d69cb5f0187d1902c0029b5e8f5d920220c64
skybax <tmflame@gmail.com>
2022-04-07 15:41:37 -0500

    Remove unneeded cup class

--- cup_manager/__init__.py
+++ cup_manager/__init__.py
@@ -5,7 +5,6 @@ from pyplanet.apps.config import AppConfig
 from .results import ResultsCupManager
 from .setup import SetupCupManager
 from .payouts import PayoutCupManager
-from .cup  import CupCupManager
 
 logger = logging.getLogger(__name__)
 
@@ -21,11 +20,9 @@ class CupManagerApp(AppConfig):
 		self.results = ResultsCupManager(self)
 		self.setup = SetupCupManager(self)
 		self.payout = PayoutCupManager(self)
-		self.cup = CupCupManager(self)
 
 
 	async def on_start(self):
 		await self.results.on_start()
 		await self.setup.on_start()
 		await self.payout.on_start()
-		await self.cup.on_start()
