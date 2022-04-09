f8955946e3a25c3175acc76610058a5b1df5d4e8
skybax <tmflame@gmail.com>
2022-04-09 12:10:09 -0500

    Fix issue when no players have a score

--- cup_manager/views/textbox_view.py
+++ cup_manager/views/textbox_view.py
@@ -157,104 +157,103 @@ class TextResultsView(TextboxView):
 	async def get_text_data(self) -> str:
 		text = ''
 		if self._instance_data:
-
 			instance_data = [item for item in self._instance_data if item.score != 0] if self.exclude_zero_points else self._instance_data
+			if instance_data:
+				if self._export_format in [ self.ExportFormat.MARKDOWN, self.ExportFormat.DISCORD ]:
 
-			if self._export_format in [ self.ExportFormat.MARKDOWN, self.ExportFormat.DISCORD ]:
+					indexes = [str(item) for item in range(1, len(instance_data) + 1)]
+					scores = [str(item.score_str) for item in instance_data]
+					score2s = [str(item.score2_str) for item in instance_data]
+					nicknames = [style.style_strip(item.nickname, style.STRIP_ALL) for item in instance_data]
+					countries = [str(item.country) for item in instance_data]
 
-				indexes = [str(item) for item in range(1, len(instance_data) + 1)]
-				scores = [str(item.score_str) for item in instance_data]
-				score2s = [str(item.score2_str) for item in instance_data]
-				nicknames = [style.style_strip(item.nickname, style.STRIP_ALL) for item in instance_data]
-				countries = [str(item.country) for item in instance_data]
+					index_justify = min(4, len(max(indexes, key=len)))
+					score_justify = min(15, len(max(scores, key=len)))
+					score2_justify = min(15, len(max(score2s, key=len)))
 
-				index_justify = min(4, len(max(indexes, key=len)))
-				score_justify = min(15, len(max(scores, key=len)))
-				score2_justify = min(15, len(max(score2s, key=len)))
+					if self._export_format == self.ExportFormat.DISCORD:
+						text += f'**{self.cup_name}** - {self.cup_edition} - {str(len(instance_data))} Players\n'
 
-				if self._export_format == self.ExportFormat.DISCORD:
-					text += f'**{self.cup_name}** - {self.cup_edition} - {str(len(instance_data))} Players\n'
+						sorted_match_info_list = sorted(self._instance_match_data, key=lambda x: x.map_start_time)
+						for match_info in sorted_match_info_list:
+							mx_id = match_info.mx_id
+							mx_base_url = ''
+							if 'mx' in self.app.instance.apps.apps:
+								try:
+									mx_base_url = self.app.instance.apps.apps['mx'].api.base_url()
+								except:
+									logger.error(f'Error determining (T)MX base url')
+							text += f'*{match_info.mode_script}* on {style.style_strip(match_info.map_name)}'
+							if mx_id and mx_base_url:
+								text += f' <{mx_base_url}/s/tr/{mx_id}>'
+							text += '\n'
 
-					sorted_match_info_list = sorted(self._instance_match_data, key=lambda x: x.map_start_time)
-					for match_info in sorted_match_info_list:
-						mx_id = match_info.mx_id
-						mx_base_url = ''
-						if 'mx' in self.app.instance.apps.apps:
-							try:
-								mx_base_url = self.app.instance.apps.apps['mx'].api.base_url()
-							except:
-								logger.error(f'Error determining (T)MX base url')
-						text += f'*{match_info.mode_script}* on {style.style_strip(match_info.map_name)}'
-						if mx_id and mx_base_url:
-							text += f' <{mx_base_url}/s/tr/{mx_id}>'
+						score_prev = ()
+						placement_emotes = [
+							':first_place:',
+							':second_place:',
+							':third_place:',
+							':four:',
+						]
+						placement_emote_index = 0
+						prev_placement_emote_index = 0
+						placement_index = 0
+
+						for nickname, country, score, score2 in zip(nicknames, countries, scores, score2s):
+							if self.display_ties and score_prev == (score, score2):
+								placement_emote_index = prev_placement_emote_index
+							if placement_emote_index >= len(placement_emotes) or (placement_index > len(placement_emotes) and placement_emote_index != prev_placement_emote_index):
+								break
+							text += f'{placement_emotes[placement_emote_index]} {country_codes.get_discord_flag(country)} {nickname}\n'
+							prev_placement_emote_index = placement_emote_index
+							score_prev = (score, score2)
+							placement_index += 1
+							placement_emote_index = placement_index
+
 						text += '\n'
+						text += 'Full results:\n'
 
 					score_prev = ()
-					placement_emotes = [
-						':first_place:',
-						':second_place:',
-						':third_place:',
-						':four:',
-					]
-					placement_emote_index = 0
-					prev_placement_emote_index = 0
-					placement_index = 0
+					index_prev = ''
 
-					for nickname, country, score, score2 in zip(nicknames, countries, scores, score2s):
+					text += "```\n"
+					for index, nickname, score, score2 in zip(indexes, nicknames, scores, score2s):
+						display_index = index
+
 						if self.display_ties and score_prev == (score, score2):
-							placement_emote_index = prev_placement_emote_index
-						if placement_emote_index >= len(placement_emotes) or (placement_index > len(placement_emotes) and placement_emote_index != prev_placement_emote_index):
-							break
-						text += f'{placement_emotes[placement_emote_index]} {country_codes.get_discord_flag(country)} {nickname}\n'
-						prev_placement_emote_index = placement_emote_index
+							display_index = index_prev
+						index_prev = display_index
 						score_prev = (score, score2)
-						placement_index += 1
-						placement_emote_index = placement_index
 
-					text += '\n'
-					text += 'Full results:\n'
+						text += str(display_index.rjust(index_justify)) + '  '
+						if self._show_score2:
+							text += str(score2.rjust(score2_justify)) + '  '
+						text += str(score.rjust(score_justify)) + '  '
+						text += str(nickname) + '\n'
 
-				score_prev = ()
-				index_prev = ''
+					if self.exclude_zero_points and self.exclude_zero_points_as_spec:
+						excluded_players = [item for item in self._instance_data if item.score == 0]
+						spec_justify = index_justify + score_justify + 4
+						if self._show_score2:
+							spec_justify += score2_justify + 2
+						for excluded_player in excluded_players:
+							text += str('Spec'.ljust(spec_justify))
+							text += str(style.style_strip(excluded_player.nickname, style.STRIP_ALL)) + '\n'
 
-				text += "```\n"
-				for index, nickname, score, score2 in zip(indexes, nicknames, scores, score2s):
-					display_index = index
-
-					if self.display_ties and score_prev == (score, score2):
-						display_index = index_prev
-					index_prev = display_index
-					score_prev = (score, score2)
-
-					text += str(display_index.rjust(index_justify)) + '  '
-					if self._show_score2:
-						text += str(score2.rjust(score2_justify)) + '  '
-					text += str(score.rjust(score_justify)) + '  '
-					text += str(nickname) + '\n'
-
-				if self.exclude_zero_points and self.exclude_zero_points_as_spec:
-					excluded_players = [item for item in self._instance_data if item.score == 0]
-					spec_justify = index_justify + score_justify + 4
-					if self._show_score2:
-						spec_justify += score2_justify + 2
-					for excluded_player in excluded_players:
-						text += str('Spec'.ljust(spec_justify))
-						text += str(style.style_strip(excluded_player.nickname, style.STRIP_ALL)) + '\n'
-
-				text += "```"
-			elif self._export_format == self.ExportFormat.CSV:
-				indexes = [str(item) for item in range(1, len(instance_data) + 1)]
-				for item, index in zip(instance_data, indexes):
-					text += '"' + str(index) + '",'
-					text += '"' + str(item.score_str) + '",'
-					if self._show_score2:
-						text += '"' + str(item.score2_str) + '",'
-					text += '"' + style.style_strip(item.nickname, style.STRIP_ALL) + '",'
-					text += '"' + str(item.login) + '",'
-					text += '"' + str(item.country) + '"\n'
-			else:
-				text = f"Export format not implemented: {str(self._export_format)}"
-				logger.error(text)
+					text += "```"
+				elif self._export_format == self.ExportFormat.CSV:
+					indexes = [str(item) for item in range(1, len(instance_data) + 1)]
+					for item, index in zip(instance_data, indexes):
+						text += '"' + str(index) + '",'
+						text += '"' + str(item.score_str) + '",'
+						if self._show_score2:
+							text += '"' + str(item.score2_str) + '",'
+						text += '"' + style.style_strip(item.nickname, style.STRIP_ALL) + '",'
+						text += '"' + str(item.login) + '",'
+						text += '"' + str(item.country) + '"\n'
+				else:
+					text = f"Export format not implemented: {str(self._export_format)}"
+					logger.error(text)
 		return text
 
 
