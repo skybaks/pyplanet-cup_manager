2e8ad7c45247c6158f3d06bdcb1391b18a6fbb65
skybax <tmflame@gmail.com>
2023-09-01 16:27:38 -0500

    Fix test starting folder

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
 
 if __name__ == '__main__':
-	tests = unittest.defaultTestLoader.discover(start_dir='', pattern='*_test.py')
+	tests = unittest.defaultTestLoader.discover(start_dir='skybaks', pattern='*_test.py')
 	unittest.TextTestRunner().run(tests)
