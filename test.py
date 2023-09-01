6918aab29d80030a1d83699eeca0f99bf80fed1f
skybax <tmflame@gmail.com>
2023-09-01 16:40:10 -0500

    Run black on test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
 
-if __name__ == '__main__':
-	tests = unittest.defaultTestLoader.discover(start_dir='skybaks', pattern='*_test.py')
-	unittest.TextTestRunner().run(tests)
+if __name__ == "__main__":
+    tests = unittest.defaultTestLoader.discover(
+        start_dir="skybaks", pattern="*_test.py"
+    )
+    unittest.TextTestRunner().run(tests)
